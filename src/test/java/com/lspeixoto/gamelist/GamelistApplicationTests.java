acf1ae04629d3dca5c60acc06657ecdc1cf450c0
Lucas Peixoto <lspeixotodev@gmail.com>
2023-05-16 05:46:40 -0300

    Config h2 database and create Game entity

---
+++ src/test/java/com/lspeixoto/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lspeixoto.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
